12519b315ee6c0c4ac3b8cc0a1c1393f93ff2191
Nikolay Botev <nikolaybotev.b@gmail.com>
2025-04-25 22:59:50 -0700

    FastReadWriteLock: streamline implementation.

--- src/main/java/com/nikolaybotev/metrics/cloudmonitoring/util/lazy/FastReadWriteLock.java
+++ src/main/java/com/nikolaybotev/metrics/cloudmonitoring/util/lazy/FastReadWriteLock.java
@@ -1,15 +1,9 @@
 package com.nikolaybotev.metrics.cloudmonitoring.util.lazy;
 
 import java.io.Serializable;
-import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class FastReadWriteLock implements Serializable {
-    public static final AtomicInteger readYield = new AtomicInteger();
-    public static final AtomicInteger writeYield = new AtomicInteger();
-    public static final AtomicInteger[] concurrentReaders =
-            Arrays.stream(new Object[200]).map(n -> new AtomicInteger()).toArray(AtomicInteger[]::new);
-
     private final AtomicInteger counter = new AtomicInteger();
     private final ReadLock readLock = new ReadLock();
     private final WriteLock writeLock = new WriteLock();
@@ -19,11 +13,8 @@ public class FastReadWriteLock implements Serializable {
 
         @Override
         public void close() {
-            var held = counter.getAndAdd(-2);
-            if (held % 2 == 1 || held < 0) {
-                throw new IllegalStateException("held by read lock " + held);
-            }
-            concurrentReaders[held / 2].incrementAndGet();
+            var prev = counter.getAndAdd(-2);
+            assert prev % 2 == 0 && prev >= 0;
         }
     }
 
@@ -32,33 +23,28 @@ public class FastReadWriteLock implements Serializable {
 
         @Override
         public void close() {
-            var held = counter.getAndDecrement();
-            if (held % 2 == 0) {
-                throw new IllegalStateException("held by write lock " + held);
-            }
+            var prev = counter.getAndDecrement();
+            assert prev % 2 == 0;
         }
     }
 
     public ReadLock acquireReadLock() {
-        while (true) {
-            // Optimistically acquire read lock
-            var previous = counter.getAndAdd(2);
-            if (previous % 2 == 1) {
-                // Write lock is held ... release our lock and yield.
-                counter.getAndAdd(-2);
-                readYield.incrementAndGet();
-                Thread.yield();
-            } else {
-                break;
-            }
+        // Optimistically acquire a read lock.
+        var previous = counter.getAndAdd(2);
+        while (previous % 2 == 1) {
+            // Write lock is held... release our lock and yield.
+            counter.getAndAdd(-2);
+            Thread.yield();
+
+            // Try again.
+            previous = counter.getAndAdd(2);
         }
         return readLock;
     }
 
     public WriteLock acquireWriteLock() {
         while (!counter.compareAndSet(0, 1)) {
-            // Write lock not possible... yield.
-            writeYield.incrementAndGet();
+            // Read or Write lock is held... yield.
             Thread.yield();
         }
         return writeLock;
